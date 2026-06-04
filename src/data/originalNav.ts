e4853da7b354def45fed4dddc7ef2679076cc51c
jmtdev0 <jmartintorres@outlook.es>
2026-06-04 20:28:54 +0200

    Make original clone header links local

---
+++ src/data/originalNav.ts
@@ -0,0 +1,9 @@
+export const originalNavItems = [
+  { label: 'Inicio', href: '/' },
+  { label: 'CARTA', href: '/menus/' },
+  { label: 'BEBIDA', href: '/menus-1/' },
+  { label: 'RESERVAR', href: '/general-clean/' },
+  { label: 'PEDIDOS', href: '/general-8/' },
+  { label: 'Contacto', href: '/contact-9/' },
+  { label: 'Experiencias', href: '/experiences/' },
+];
